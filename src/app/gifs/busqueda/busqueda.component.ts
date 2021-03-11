c5b17432e1a8d129000c2b6600146a1c2a8fadd4
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-03-11 15:29:26 -0500

    crear servicio para el historial

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,7 +7,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styles: [],
 })
 export class BusquedaComponent implements OnInit {
-  constructor() {}
+  constructor(private gifsService: GifsService) {}
 
   ngOnInit(): void {}
 
@@ -15,6 +16,7 @@ export class BusquedaComponent implements OnInit {
   buscar(): void {
     const valor = this.txtBuscar.nativeElement.value;
     this.txtBuscar.nativeElement.value = '';
-    console.log(valor);
+
+    this.gifsService.buscarGifs(valor);
   }
 }
